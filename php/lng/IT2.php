0e301987725dfb6b7e82348a2d938d24dea103e7
jumpjack <jumpjack@libero.it>
2021-10-09 15:59:43 +0200

    aggiunta versione php

---
+++ php/lng/IT2.php
@@ -0,0 +1,59 @@
+<?php
+$weather_api_lng											=				'it';
+$lng['Update']												= 				'Aggiorna';
+$lng['Preconditioning requested.']							=				'Richiesta preconsizionamento inviata.';
+$lng['Instant charging requested.']							=				'Richiesta avvio ricarica inviata.';
+$lng['Activation of the charging schedule requested.']		=				'Richiesata ricarica programmata inviata.';
+$lng['Deactivation of the charging schedule requested.']	=				'Richiesta disattovaione ricarica programmata inviata.';
+$lng['No new data']											=				'Nessun dato aggiornato ricevuto.';
+$lng['Mileage']												=				'km totali';
+$lng['Connected']											=				'Connesso';
+$lng['No'													=				'No';
+$lng['Yes']													=				'S&igrave';  										//10
+$lng['Charging']											=				'Ricarica in corso';
+$lng['Soon']												=				'Presto';
+$lng['Ready']												=				'Ready';
+$lng['Effect']												=				'Effect';
+$lng['Charging schedul']													'Charging schedule';
+$lng['Inactive']											=				'Inactive';
+$lng['Active']												=				'Active';
+$lng['Battery level']										=				'Battery level';
+$lng['Action at battery level']								=						'Action at battery level';
+$lng['Battery capacity']													'Battery capacity'; 					 // 20
+$lng['Range']												=				'Range';
+$lng['Battery temperature']									=						'Battery temperature';
+$lng['Outside temperature']									=						'Outside temperature';
+$lng['Status update']										=				'Status update';
+$lng['Car position']										=				'Car position';
+$lng['Position update']															'Position update';
+$lng['Start preconditioning']								=						'Start preconditioning';
+$lng['on']													=						'o
+$lng['off']													=						'		;
+$lng['Start charging']															'Start charging';                      // 30
+$lng['some']												=						'		';
+$lng['Specified battery level re		d.']				=						'Specified battery level reached.';
+$lng['Battery level']										=				'Battery level';
+$lng['Remaining charging time']								=						'Remaining charging time';
+$lng['minutes']												=						inutes';
+$lng['Range']												=								ge';
+$lng['Status update']										=				'Status update';
+$lng[ 'Charging finished.']									=						'Charging finished.';
+$lng['Charging history']														'Charging history';
+$lng['Start']												=								rt';                                        // 40
+$lng['Charging']											=						harging';
+$lng['to']													=						't
+$lng['in']													=						'i
+$lng['minutes']												=						inutes';
+$lng['Power']												=								er';
+$lng['Status']												=							atus';
+$lng['at']													=						'a
+$lng['Back']												=						'		';
+$lng['Date']												=						'		';
+$lng['Count']												=								nt';
+$lng['kWh']													=						'		;
+$lng['Range by petrol']															'Range by petrol';
+$lng['Range by battery']														'Range by battery';
+$lng['Total range']											=				'Total range';
+$lng['Hvac status']											=				'Hvac status';
+$lng['Hvac last update']																'Hvac last update';
+?>
